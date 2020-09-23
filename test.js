4b15ba680383031b9dc0088e7c18df2c75af49f8
fedemengo <19249682+fedemengo@users.noreply.github.com>
2020-09-23 16:35:08 +0200

    Add github config

--- test.js
+++ test.js
@@ -4,14 +4,17 @@ const chai = require('chai')
 const v = require('./index')
 
 describe('Test avatar generation', () => {
-    const helloURL = 'http://vanillicon.com/v2/5d41402abc4b2a76b9719d911017c592.svg'
+    const helloURL = 'https://vanillicon.com/v2/5d41402abc4b2a76b9719d911017c592.svg'
     it(`Should return ${helloURL}`, () => {
         return v.generate('hello').then(url => chai.assert(url === helloURL))
     })
 
-    const lolURL = 'http://vanillicon.com/v2/9cdfb439c7876e703e307864c9167a15.svg'
+    const lolURL = 'https://vanillicon.com/v2/9cdfb439c7876e703e307864c9167a15.svg'
     it(`Should return ${lolURL}`, () => {
-        return v.generate('lol').then(url => chai.assert(url === lolURL))
+        return v.generate('lol').then(url => {
+			console.log(url);
+			return chai.assert(url === lolURL)
+		})
     })
 })
 
